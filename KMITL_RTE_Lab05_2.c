be7b5ada7e91e4457fbd5d6cdf27dc743925c01c
Bourbxn <jakkapat97@gmail.com>
2021-09-06 20:46:47 +0700

    MSI

---
+++ KMITL_RTE_Lab05_2.c
@@ -0,0 +1,10 @@
+#include "stdio.h"
+int main(){
+  char sentc[10000];
+  scanf("%[^\n]",&sentc);
+  int i=0;
+  while(sentc[++i]!='\0');
+  for(int j=i-1;j>=0;j--){
+    printf("%c", sentc[j]);
+  }
+}
