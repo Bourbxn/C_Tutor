a18d7b35ff8dde556d346ec6ae3cf6763f43670d
Jakkapat Paijit <jakkapat97@gmail.com>
2022-03-15 10:54:18 +0700

    Update KMITL_ME_Lab11_Q5.c

--- KMITL_ME_Lab11_Q5.c
+++ KMITL_ME_Lab11_Q5.c
@@ -4,4 +4,5 @@ int main(){
 	printf("Input 5 characters of string : ");
 	scanf("%s",input);
 	printf("   \n  %c\n%c\n   %c\n %c\n    %c",input[2],input[0],input[3],input[1],input[4]);
+	return 0;
 }
