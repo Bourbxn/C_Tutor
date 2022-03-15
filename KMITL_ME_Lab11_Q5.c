1685fd90454bbdd03237ed5e2af598c8f7ddd32c
Jakkapat Paijit <jakkapat97@gmail.com>
2022-03-15 09:14:55 +0700

    Macbook

---
+++ KMITL_ME_Lab11_Q5.c
@@ -0,0 +1,7 @@
+#include "stdio.h"
+int main(){
+	char input[5];
+	printf("Input 5 characters of string : ");
+	scanf("%s",input);
+	printf("   \n  %c\n%c\n   %c\n %c\n    %c",input[2],input[0],input[3],input[1],input[4]);
+}
