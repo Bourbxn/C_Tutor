d34d087cb89980a295fcb6abb331c9e6d3bd61af
Bourbxn <jakkapat97@gmail.com>
2022-03-20 23:29:20 +0700

    Macbook

---
+++ KMITL_ME_Lab09_3.c
@@ -0,0 +1,56 @@
+#include "stdio.h"
+void isPalindrome(char* string)
+{
+	char *p1, *p2;
+
+	int i=0,length = 0;
+	while(string[i++]!='\0') length++;
+	char newStr[length+1];
+	int index = 0;
+	for(int i=0;i<length;i++){
+		if(string[i]>='A' && string[i]<='Z'){
+			newStr[index++] = string[i];
+		}
+		else if(string[i]>='a' && string[i]<='z'){
+			newStr[index++] = string[i] - ('a'-'A');
+		}
+		else if(string[i]>='0' && string[i]<='9'){
+			newStr[index++] = string[i];
+		}
+		else{
+			continue;
+		}
+	}
+	newStr[length] = '\0';
+
+	p1 = newStr;
+	while (*p1 != '\0') {
+		++p1;
+	}
+	--p1;
+
+	for (p2 = newStr; p1 >= p2;) {
+		if (*p1 == *p2) {
+			--p1;
+			p2++;
+		}
+		else
+			break;
+	}
+
+	if (p2 > p1)
+		printf("\"%s\" is PALINDROME.",string);
+	else
+		printf("\"%s\" is NOT a Palindrom.",string);
+}
+
+int main()
+{
+	char str[100];
+	printf(" *** Palindrome Verification ***\n");
+	printf("Enter a sentence : ");
+	scanf("%[^\n]",str);
+	isPalindrome(str);
+
+	return 0;
+}
