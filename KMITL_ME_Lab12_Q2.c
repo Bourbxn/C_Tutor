893cadbdb7872567ee83545a75b6323c6a5d9cbf
Bourbxn <jakkapat97@gmail.com>
2022-03-28 13:59:36 +0700

    Macbook

--- KMITL_ME_Lab12_Q2.c
+++ KMITL_ME_Lab12_Q2.c
@@ -1,9 +1,15 @@
 #include "stdio.h"
 int main(){
-	int num1,num2;
+	int num1,num2,temp;
 	printf(" *** Display Summation (skip 2 number)***\n");
 	printf("Enter 2 whole numbers : ");
 	scanf("%d %d",&num1,&num2);
+	if(num1>num2){
+		printf("Loop");
+		temp = num1;
+		num1 = num2;
+		num2 = temp;
+	}
 	int sum = 0;
 	printf("Summation = ");
 	for(int i=num1;i<=num2;i+=2){
